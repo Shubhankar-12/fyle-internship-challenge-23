3c54abef813f15a91aa9e6078b60ae14d8afd6e6
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-11-08 23:04:22 +0530

    env changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Octokit } from 'octokit';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 const octokit = new Octokit({
   auth: environment.githubToken,
